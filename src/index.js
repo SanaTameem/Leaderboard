e0b13135ced358e7608c34e0d4777049330a9091
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-06-23 13:12:57 +0430

    Update index.html file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import './style.css';
 import postScore from './modules/post.js';
 import isNumeric from './modules/onlyNumber.js';
 import scoreDiv from './modules/loadData.js';
-
-const alertSpan = document.querySelector('.alert');
 const nameInput = document.querySelector('.name-input');
 const scoreInput = document.querySelector('.score-input');
 const form = document.querySelector('.add-score-form');
@@ -15,14 +13,7 @@ form.addEventListener('submit', (e) => {
     if (isNumeric(scoreInput.value)) {
       postScore(nameInput.value, scoreInput.value);
       form.reset();
-    } else {
-      alertSpan.textContent = 'Please write a valid Name and Score';
-      alertSpan.style.display = 'flex';
-      setTimeout(() => {
-        alertSpan.style.display = 'none';
-      }, 3000);
-      scoreInput.value = '';
-    }
+    } 
   }
 });
 
